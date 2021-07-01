10d0e8f2909aae29f643634c5920151362327669
sjahanmard <86264097+sjahanmard@users.noreply.github.com>
2021-07-01 20:10:15 +0430

    as

--- practice.js
+++ practice.js
@@ -2,11 +2,9 @@
 const sal = function (num) {
   num * num;
 };
-
-
 const sal = (num) => num * num;
 console.log(sal);
-/*template literal*/
+/*template literal: use back tick to put dynamic variables in there*/
 const string = `this is a word ${faradars}`;
 /*destructuring*/
 const stundet = {
@@ -16,7 +14,7 @@ const stundet = {
 student.firstname
 
 const {firstname , lastname} = student;
-firstname
+// then just call it like : firstname
 /*ternary operator*/
 let age = 18
 if (age>=18) {
@@ -36,7 +34,10 @@ import boob from "./javascript.js"
 /*map*/
 const myArray = [1,2,3,4]
 const newArray = myArray.map((value,index,array)=> value*2);
-
+// Person is a function that we imported here and persons is a list
+const personList = persons.map (person => <Person key={person.id} person={person} />)
+// at the end of the above line weare setting a property with the name person
+// which is exactly our own person
 /*filter*/
 const myArray = [1,2,3,4]
 const newArray = myArray.filter((value,index,array) => value%2 == 0);
